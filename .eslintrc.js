537d768ba322b4124fbec6d413d8e4e98bab432e
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-01-11 20:54:22 +0530

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': ['error'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.{js,jsx}', '**/*.test.{js,jsx}', '**/*.spec.{js,jsx}'] },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+};
