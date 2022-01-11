b1bd167d30afe2afd9d8a49dc70dffd845e8412e
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-01-11 22:53:19 +0530

    Created Typography comp for typography

---
+++ src/components/Typography/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from 'react';
+import './index.css';
+
+const variantsMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  body1: 'p',
+  body2: 'p',
+};
+
+const Typography = ({ children, component, variant, ...props }) => {
+  const Component = variantsMapping[component] ?? 'p';
+
+  return (
+    <Component {...props} className={`Typography__root Typography--${variant}`}>
+      {children}
+    </Component>
+  );
+};
+
+export default Typography;
